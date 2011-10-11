5ae5373615a3b7e2e4eed2ffbf537a484554a3f8
Martin May <martin@martinmay.net>
2011-10-11 14:52:38 -0600

    use proper param name

--- lib/skittles/connection.rb
+++ lib/skittles/connection.rb
@@ -12,7 +12,7 @@ module Skittles
 		  	client = OAuth2::Client.new(client_id, client_secret, options)
 		  	oauth_token = OAuth2::AccessToken.new(client, access_token)
 		  	
-		  	oauth_token.token_param = 'oauth_token'
+		  	oauth_token.param_name = 'oauth_token'
 		  	oauth_token
 		  end
 	end
